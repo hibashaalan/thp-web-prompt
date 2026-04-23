fccdcae00e6d2aa193a9a644fbb818bbb9cb8015
Hibatullah Shaalan <hshaalan@oberlin.edu>
2026-04-23 17:31:19 -0400

    update

--- app/api/signout/route.ts
+++ app/api/signout/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/lib/supabase/server'
+
 export async function GET(request: Request) {
   const supabase = await createClient()
   await supabase.auth.signOut()
